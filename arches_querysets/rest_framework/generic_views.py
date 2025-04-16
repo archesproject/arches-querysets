213dabf0650b0993e9b25e19934ae7df7437ae75
Jacob Walls <jwalls@fargeo.com>
2025-04-16 17:04:11 -0400

    Move MultiPartParser

--- arches_querysets/rest_framework/generic_views.py
+++ arches_querysets/rest_framework/generic_views.py
@@ -11,18 +11,34 @@ from arches_querysets.rest_framework.view_mixins import ArchesModelAPIMixin
 class ArchesResourceListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
     permission_classes = [ResourceEditor | ReadOnly]
     serializer_class = ArchesResourceSerializer
+    parser_classes = [
+        "rest_framework.parsers.JSONParser",
+        "arches_querysets.rest_framework.multipart_json_parser.MultiPartJSONParser",
+    ]
 
 
 class ArchesResourceDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [ResourceEditor | ReadOnly]
     serializer_class = ArchesResourceSerializer
+    parser_classes = [
+        "rest_framework.parsers.JSONParser",
+        "arches_querysets.rest_framework.multipart_json_parser.MultiPartJSONParser",
+    ]
 
 
 class ArchesTileListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
     permission_classes = [ResourceEditor | ReadOnly]
     serializer_class = ArchesTileSerializer
+    parser_classes = [
+        "rest_framework.parsers.JSONParser",
+        "arches_querysets.rest_framework.multipart_json_parser.MultiPartJSONParser",
+    ]
 
 
 class ArchesTileDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [ResourceEditor | ReadOnly]
     serializer_class = ArchesTileSerializer
+    parser_classes = [
+        "rest_framework.parsers.JSONParser",
+        "arches_querysets.rest_framework.multipart_json_parser.MultiPartJSONParser",
+    ]
