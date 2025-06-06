685b432ae38c1313cd886678b3f7573fd8425067
Jacob Walls <jwalls@fargeo.com>
2025-06-06 12:54:34 -0400

    Provide display name with resource interchange value

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -12,11 +12,10 @@ class DatatypeTests(GraphTestCase):
             "datatype_lookups", as_representation=True
         )
         cls.semantic_resource = resources.get(pk=cls.resource.pk)
+        cls.datatype_1 = cls.semantic_resource.aliased_data.datatypes_1
+        cls.datatype_n = cls.semantic_resource.aliased_data.datatypes_n
 
-    def test_as_representation(self):
-        datatype_1 = self.semantic_resource.aliased_data.datatypes_1
-        datatype_n = self.semantic_resource.aliased_data.datatypes_n
-
+    def test_as_representation_display_values(self):
         display_values = {
             # Start with the tile representation.
             **self.sample_data_1,
@@ -39,20 +38,48 @@ class DatatypeTests(GraphTestCase):
             "url": json.dumps(self.sample_data_1["url"]),
         }
 
-        # TODO: assert values -- notice concepts still have a Value instance
-        # values = {}
-
         # The representation is available on the nodegroup .aliased_data.
         for datatype, representation in display_values.items():
             node_alias = datatype.replace("-", "_")
-            with self.subTest(datatype=datatype):
-                aliased_data = getattr(datatype_1.aliased_data, node_alias)
-                self.assertEqual(
-                    aliased_data.get("display_value"),
-                    representation,
-                )
-                aliased_data_n = getattr(datatype_n[0].aliased_data, node_alias + "_n")
-                self.assertEqual(
-                    aliased_data_n.get("display_value"),
-                    representation,
-                )
+            for aliased_data, cardinality in [
+                (self.datatype_1.aliased_data, "1"),
+                (self.datatype_n[0].aliased_data, "n"),
+            ]:
+                with self.subTest(datatype=datatype, cardinality=cardinality):
+                    lookup = node_alias if cardinality == "1" else node_alias + "_n"
+                    value = getattr(aliased_data, lookup)
+                    self.assertEqual(value.get("display_value"), representation)
+
+    def test_as_representation_interchange_values(self):
+        # TODO: assert values -- notice concepts still have a Value instance
+        interchange_values = {
+            # Start with the tile representation.
+            **self.sample_data_1,
+            # Some interchange values are different.
+            # String resolves to active language.
+            "string": "forty-two",
+            # Resource Instance{list} resolves to stringified UUID. (TODO: unstring?)
+            "resource-instance": [
+                {
+                    "resourceId": str(self.resource.pk),
+                    "display_value": self.resource.descriptors["en"]["name"],
+                }
+            ],
+            # Concept{list} resolves to concept value.
+            # TODO...
+        }
+        interchange_values["resource-instance-list"] = interchange_values[
+            "resource-instance"
+        ]
+
+        # The interchange value is available on the nodegroup .aliased_data.
+        for datatype, interchange_value in interchange_values.items():
+            node_alias = datatype.replace("-", "_")
+            for aliased_data, cardinality in [
+                (self.datatype_1.aliased_data, "1"),
+                (self.datatype_n[0].aliased_data, "n"),
+            ]:
+                with self.subTest(datatype=datatype, cardinality=cardinality):
+                    lookup = node_alias if cardinality == "1" else node_alias + "_n"
+                    value = getattr(aliased_data, lookup)
+                    self.assertEqual(value.get("interchange_value"), interchange_value)
