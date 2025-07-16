7b0acae8ce2f572ce06f16bfec7963fe424de26e
Christopher Byrd <chrabyrd@gmail.com>
2025-07-16 10:37:40 -0700

    rough out

--- arches_querysets/rest_framework/generic_views.py
+++ arches_querysets/rest_framework/generic_views.py
@@ -1,5 +1,10 @@
+import json
+
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.parsers import JSONParser
+from rest_framework.parsers import JSONParser, MultiPartParser
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
+from rest_framework import status
 
 from arches_querysets.rest_framework.multipart_json_parser import MultiPartJSONParser
 from arches_querysets.rest_framework.pagination import ArchesLimitOffsetPagination
@@ -34,4 +39,24 @@ class ArchesTileListCreateView(ArchesModelAPIMixin, ListCreateAPIView):
 class ArchesTileDetailView(ArchesModelAPIMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [ResourceEditor | ReadOnly]
     serializer_class = ArchesTileSerializer
-    parser_classes = [JSONParser, MultiPartJSONParser]
+
+    parser_classes = [MultiPartParser, JSONParser]
+
+    def update(self, request, *args, **kwargs):
+        raw_json = request.data.get("json")
+
+        if not raw_json:
+            raise ParseError(detail="Missing 'json' part in multipart payload")
+
+        try:
+            parsed_data = json.loads(raw_json)
+        except ValueError as exc:
+            raise ParseError(detail=f"Invalid JSON in 'json' part: {exc}")
+        
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=parsed_data, partial=True)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
