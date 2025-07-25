archesproject/arches-querysets

commit 73d3d97f745c4bf7589d4914d0a843a8fc24b6c6

Remove dummy save methods, shorten transactions

arches_querysets/models.py
@@ -92,8 +92,7 @@ class ResourceTileTree(ResourceInstance):
         self._aliased_data = value
 
     def save(self, *, request=None, index=True, **kwargs):
-        with transaction.atomic():
-            self._save_aliased_data(request=request, index=index, **kwargs)
+        self._save_aliased_data(request=request, index=index, **kwargs)
 
     @classmethod
     def get_tiles(
@@ -145,9 +144,6 @@ class ResourceTileTree(ResourceInstance):
         ephemeral_proxy_instance.save_edit(
             user=user, edit_type=edit_type, transaction_id=transaction_id
         )
-
-    def save_without_aliased_data(self, **kwargs):
-        return super().save(**kwargs)
 
     def refresh_from_db(self, using=None, fields=None, from_queryset=None, user=None):
         del self._tile_trees
@@ -247,10 +243,10 @@ class TileTree(TileModel):
                 pk=models.F("nodegroup")
             )
         request = request or self._request
-        with transaction.atomic():
-            if self.sortorder is None or self.is_fully_provisional():
-                self.set_next_sort_order()
-            self._save_aliased_data(request=request, index=index, **kwargs)
+        # Mimic some computations trapped on TileModel.save().
+        if self.sortorder is None or self.is_fully_provisional():
+            self.set_next_sort_order()
+        self._save_aliased_data(request=request, index=index, **kwargs)
 
     @classmethod
     def get_tiles(
@@ -335,7 +331,7 @@ class TileTree(TileModel):
         if self.nodegroup_id and hasattr(self.nodegroup, "grouping_node"):
             return self.nodegroup.grouping_node.alias
         if not getattr(self, "_nodegroup_alias", None):
-            self._nodegroup_alias = Node.objects.get(pk=self.nodegroup_id).alias
+            self._nodegroup_alias = self.nodegroup.alias if self.nodegroup_id else None
         return self._nodegroup_alias
 
     @classmethod
@@ -528,15 +524,6 @@ class TileTree(TileModel):
             # Future: upstream this into datatype methods (another hook?)
             pair["display_value"] = _("(Empty)")
         return pair
-
-    def save_without_aliased_data(self, **kwargs):
-        return super().save(**kwargs)
-
-    def dummy_save(self, **kwargs):
-        """Don't save this tile, but run any other side effects."""
-        # update_fields=set() will abort the save.
-        save_kwargs = {**kwargs, "update_fields": set()}
-        return super().save(**save_kwargs)
 
     def set_aliased_data(self, node, node_value):
         """Format node_value according to the self._as_representation flag and

arches_querysets/bulk_operations/tiles.py
@@ -224,6 +224,7 @@ class TileTreeOperation:
                     delete_siblings=True,
                 )
             self._validate_and_patch_incoming_values(tile, nodes=nodes)
+            tile.set_missing_keys_to_none()
 
         for tile in to_insert | to_update:
             # Remove no-op upserts.
@@ -287,7 +288,7 @@ class TileTreeOperation:
         return pairs
 
     def _validate_and_patch_incoming_values(self, tile, *, nodes):
-        """Validate data found on ._incoming_tile and move it to .data.
+        """Validate data found on tile._incoming_tile and move it to tile.data.
         Update errors_by_node_alias in place."""
         from arches_querysets.models import AliasedData, TileTree
 
@@ -380,6 +381,11 @@ class TileTreeOperation:
         )
 
         with transaction.atomic():
+            if isinstance(self.entry, ResourceInstance):
+                super(ResourceInstance, self.entry).save(**self.save_kwargs)
+            # no else: if the entry point needs saving, it's already in
+            # self.to_update or self.to_insert
+
             # Interact with the database in bulk as much as possible, but
             # run certain side effects from Tile.save() one-at-a-time until
             # proxy model methods can be refactored. Then run in bulk.
@@ -447,15 +453,11 @@ class TileTreeOperation:
             if self.to_update:
                 TileModel.objects.bulk_update(
                     self.to_update,
+                    # No updates to resource instance or nodegroup.
                     {"data", "parenttile", "provisionaledits", "sortorder"},
                 )
             if self.to_delete:
                 TileModel.objects.filter(pk__in=[t.pk for t in self.to_delete]).delete()
-
-            if isinstance(self.entry, ResourceInstance):
-                self.entry.save_without_aliased_data(**self.save_kwargs)
-            else:
-                self.entry.dummy_save(**self.save_kwargs)
 
             for upsert_tile in upserts:
                 if arches_version < (8, 0):

commit 964c9ae029180037f14df0a11c783c4b36b49942

Fix 7.6 fallback code for last-minute nodegroup alias derivation
Regression in 73d3d97.

arches_querysets/models.py
@@ -382,7 +382,10 @@ class TileTree(TileModel, AliasedDataMixin):
         if self.nodegroup_id and hasattr(self.nodegroup, "grouping_node"):
             return self.nodegroup.grouping_node.alias
         if not getattr(self, "_nodegroup_alias", None):
-            self._nodegroup_alias = self.nodegroup.alias if self.nodegroup_id else None
+            # But perform a last-minute check just in case. This will
+            # also run if the node alias is null.
+            if grouping_node := Node.objects.first(pk=self.nodegroup_id):
+                self._nodegroup_alias = grouping_node.alias
         return self._nodegroup_alias
 
     @classmethod

arches_querysets/querysets.py
@@ -195,7 +195,9 @@ class TileTreeQuerySet(models.QuerySet):
         child_tiles = getattr(tile, "_tile_trees", [])
         for child_tile in sorted(child_tiles, key=attrgetter("sortorder")):
             child_nodegroup_alias = child_tile.find_nodegroup_alias()
-            if child_tile.nodegroup.cardinality == "1":
+            if child_tile.nodegroup.cardinality == "1" and child_nodegroup_alias:
+                # TODO(arches_version): remove `and child_nodegroup_alias`
+                # which can no longer be null as of v8.
                 setattr(tile.aliased_data, child_nodegroup_alias, child_tile)
             else:
                 children = getattr(tile.aliased_data, child_nodegroup_alias, [])