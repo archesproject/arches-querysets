8eeed7298c8003f4c8f0a2cd09072dcb8a0c243d
Jacob Walls <jwalls@fargeo.com>
2025-07-30 14:22:02 -0400

    Add support for values()/values_list() queries

--- tests/test_lookups.py
+++ tests/test_lookups.py
@@ -1,11 +1,18 @@
-from arches_querysets.models import ResourceTileTree
+from arches_querysets.models import ResourceTileTree, TileTree
 from arches_querysets.utils.tests import GraphTestCase
 
 
-class LookupTests(GraphTestCase):
-    def test_cardinality_1_lookups(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
+class GenericLookupTests(GraphTestCase):
+    def setUp(self):
+        self.resources = ResourceTileTree.get_tiles("datatype_lookups")
+        self.tiles_1 = TileTree.get_tiles(
+            "datatype_lookups", nodegroup_alias="datatypes_1"
+        )
+        self.tiles_n = TileTree.get_tiles(
+            "datatype_lookups", nodegroup_alias="datatypes_n"
+        )
 
+    def test_cardinality_1(self):
         # Exact
         for lookup, value in [
             ("boolean", True),
@@ -23,11 +30,9 @@ class LookupTests(GraphTestCase):
             ("node_value", str(self.cardinality_1_tile.pk)),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
 
-    def test_cardinality_n_lookups(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
-
+    def test_cardinality_n(self):
         # Exact
         for lookup, value in [
             ("boolean_n__contains", [True]),
@@ -40,11 +45,27 @@ class LookupTests(GraphTestCase):
             ("node_value_n__contains", [str(self.cardinality_n_tile.pk)]),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
 
-    def test_localized_string_lookups(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
+    def test_cardinality_1_values(self):
+        for node in self.data_nodes_1:
+            with self.subTest(alias=node.alias):
+                self.assertTrue(self.resources.values(node.alias))
+                self.assertTrue(self.tiles_1.values(node.alias))
+                self.assertTrue(self.resources.values_list(node.alias))
+                self.assertTrue(self.tiles_1.values_list(node.alias))
 
+    def test_cardinality_n_values(self):
+        for node in self.data_nodes_n:
+            with self.subTest(alias=node.alias):
+                self.assertTrue(self.resources.values(node.alias))
+                self.assertTrue(self.tiles_n.values(node.alias))
+                self.assertTrue(self.resources.values_list(node.alias))
+                self.assertTrue(self.tiles_n.values_list(node.alias))
+
+
+class LocalizedStringLookupTests(GenericLookupTests):
+    def test_cardinality_1(self):
         for lookup, value in [
             ("string__any_lang_startswith", "forty"),
             ("string__any_lang_istartswith", "FORTY"),
@@ -52,7 +73,7 @@ class LookupTests(GraphTestCase):
             ("string__any_lang_icontains", "FORT"),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
 
         # Negatives
         for lookup, value in [
@@ -62,11 +83,9 @@ class LookupTests(GraphTestCase):
             ("string__any_lang_icontains", "ORTY-THREE"),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertFalse(resources.filter(**{lookup: value}))
+                self.assertFalse(self.resources.filter(**{lookup: value}))
 
-    def test_localized_string_lookups_n(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
-
+    def test_cardinality_n(self):
         for lookup, value in [
             ("string_n__any_lang_startswith", "forty"),
             ("string_n__any_lang_istartswith", "FORTY"),
@@ -74,7 +93,7 @@ class LookupTests(GraphTestCase):
             ("string_n__any_lang_icontains", "FORT"),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
 
         # Negatives
         for lookup, value in [
@@ -84,24 +103,22 @@ class LookupTests(GraphTestCase):
             ("string_n__any_lang_icontains", "ORTY-THREE"),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertFalse(resources.filter(**{lookup: value}))
+                self.assertFalse(self.resources.filter(**{lookup: value}))
 
-    def test_resource_instance_lookups(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
 
+class ResourceInstanceLookupTests(GenericLookupTests):
+    def test_cardinality_1(self):
         for lookup, value in [
             ("resource_instance__id", str(self.resource_42.pk)),
             ("resource_instance_list__contains", str(self.resource_42.pk)),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
 
-    def test_resource_instance_lookups_n(self):
-        resources = ResourceTileTree.get_tiles("datatype_lookups")
-
+    def test_cardinality_n(self):
         for lookup, value in [
             ("resource_instance_n__ids_contain", str(self.resource_42.pk)),
             # ("resource_instance_list_n__ids__contain", str(self.resource_42.pk)),
         ]:
             with self.subTest(lookup=lookup, value=value):
-                self.assertTrue(resources.filter(**{lookup: value}))
+                self.assertTrue(self.resources.filter(**{lookup: value}))
