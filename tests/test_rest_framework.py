8b683315545379a1840551daf7016af3a50a0479
Jacob Walls <jwalls@fargeo.com>
2025-11-21 15:24:15 -0500

    Add tests, safety, ensure edit log behavior is consistent on 7.6/8.0 (#112)
    * Return tile ids on create
    
    * Add test
    
    * Add failing test
    
    * Fix fill_blanks=true option
    
    * Fix edit log, transaction ids, reduce serializer drift #114 (#115)
    
    * Fix edit log, transaction ids, reduce serializer drift #114
    
    * nit
    
    * nit
    
    * nits
    
    * Remove test skip
    
    * Ensure 7.6 & 8.0 parity
    
    * Optimize test

--- tests/test_rest_framework.py
+++ tests/test_rest_framework.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 from http import HTTPStatus
 from unittest.mock import patch
 
@@ -54,6 +55,7 @@ class RestFrameworkTests(GraphTestCase):
         # The response includes the context.
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertIn("aliased_data", response.json())
+        self.assertIsInstance(uuid.UUID(response.json()["tileid"]), uuid.UUID)
         self.assertEqual(
             response.json()["aliased_data"]["string_alias_n"],
             {
@@ -89,6 +91,7 @@ class RestFrameworkTests(GraphTestCase):
             create_url, request_body, content_type="application/json"
         )
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertIsInstance(uuid.UUID(response.json()["tileid"]), uuid.UUID)
         self.assertEqual(response.json()["resourceinstance"], str(self.resource_42.pk))
         self.assertEqual(
             response.json()["aliased_data"]["string_alias_n"],
@@ -96,6 +99,130 @@ class RestFrameworkTests(GraphTestCase):
                 "display_value": "create_value",
                 "node_value": {
                     "en": {"value": "create_value", "direction": "ltr"},
+                },
+                "details": [],
+            },
+        )
+
+    def test_create_nested_tiles_for_new_resource(self):
+        self.client.login(username="dev", password="dev")
+        create_url = reverse(
+            "arches_querysets:api-tiles",
+            kwargs={"graph": "datatype_lookups", "nodegroup_alias": "datatypes_1"},
+        )
+        request_body = {
+            "aliased_data": {
+                "string_alias": "create_value",
+                "datatypes_1_child": {
+                    "aliased_data": {"string_alias_child": "child_create_value"}
+                },
+            },
+        }
+
+        response = self.client.post(
+            create_url, request_body, content_type="application/json"
+        )
+
+        # The response includes the context.
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertIn("aliased_data", response.json())
+        self.assertIsInstance(uuid.UUID(response.json()["tileid"]), uuid.UUID)
+        parent_data = response.json()["aliased_data"]
+        self.assertEqual(
+            parent_data["datatypes_1_child"]["aliased_data"]["string_alias_child"],
+            {
+                "display_value": "child_create_value",
+                "node_value": {
+                    "en": {"value": "child_create_value", "direction": "ltr"},
+                },
+                "details": [],
+            },
+        )
+        self.assertEqual(response.status_code, HTTPStatus.CREATED, response.content)
+
+        self.assertSequenceEqual(
+            EditLog.objects.filter(
+                resourceinstanceid=response.json()["resourceinstance"],
+            )
+            .values_list("edittype", flat=True)
+            .order_by("edittype"),
+            ["create", "tile create", "tile create"],
+        )
+
+    def test_create_nested_tiles_for_new_resource_via_resource_serializer(self):
+        self.client.login(username="dev", password="dev")
+        create_url = reverse(
+            "arches_querysets:api-resources",
+            kwargs={"graph": "datatype_lookups"},
+        )
+        request_body = {
+            "aliased_data": {
+                "datatypes_1": {
+                    "aliased_data": {
+                        "string_alias": "create_value",
+                        "datatypes_1_child": {
+                            "aliased_data": {"string_alias_child": "child_create_value"}
+                        },
+                    },
+                },
+            },
+        }
+
+        response = self.client.post(
+            create_url, request_body, content_type="application/json"
+        )
+
+        # The response includes the context.
+        self.assertEqual(response.status_code, HTTPStatus.CREATED, response.json())
+        parent_data = response.json()["aliased_data"]["datatypes_1"]["aliased_data"]
+        self.assertEqual(
+            parent_data["datatypes_1_child"]["aliased_data"]["string_alias_child"],
+            {
+                "display_value": "child_create_value",
+                "node_value": {
+                    "en": {"value": "child_create_value", "direction": "ltr"},
+                },
+                "details": [],
+            },
+        )
+        self.assertEqual(response.status_code, HTTPStatus.CREATED, response.content)
+
+        edits = EditLog.objects.filter(
+            resourceinstanceid=response.json()["resourceinstanceid"],
+        ).order_by("edittype")
+        self.assertEqual(
+            [edit.edittype for edit in edits], ["create", "tile create", "tile create"]
+        )
+        self.assertEqual(len(set([edit.transactionid for edit in edits])), 1)
+
+    def test_update_tile(self):
+        update_url = reverse(
+            "arches_querysets:api-tile",
+            kwargs={
+                "graph": "datatype_lookups",
+                "nodegroup_alias": "datatypes_1",
+                "pk": self.resource_42.aliased_data.datatypes_1.pk,
+            },
+        )
+        request_body = {
+            "aliased_data": {"string_alias": "update_value"},
+            "resourceinstance": str(self.resource_42.pk),
+        }
+        assert "tileid" not in request_body, "tileid is not required in update requests"
+
+        self.client.login(username="dev", password="dev")
+        response = self.client.patch(
+            update_url, request_body, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK, response.json())
+        self.assertIsInstance(uuid.UUID(response.json()["tileid"]), uuid.UUID)
+        self.assertEqual(response.json()["resourceinstance"], str(self.resource_42.pk))
+        self.assertEqual(
+            response.json()["aliased_data"]["string_alias"],
+            {
+                "display_value": "update_value",
+                "node_value": {
+                    "en": {"value": "update_value", "direction": "ltr"},
                 },
                 "details": [],
             },
@@ -153,7 +280,17 @@ class RestFrameworkTests(GraphTestCase):
             data=static_data,
         )
         self.assertTrue(serializer.is_valid())
-        # serializer.save() etc
+
+        # Or, submit it to the API
+        self.client.login(username="dev", password="dev")
+        create_url = reverse(
+            "arches_querysets:api-tiles",
+            kwargs={"graph": "datatype_lookups", "nodegroup_alias": "datatypes_1"},
+        )
+        response = self.client.post(
+            create_url, serializer.data, content_type="application/json"
+        )
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
 
     def test_exclude_children_option(self):
         serializer = ArchesResourceSerializer(graph_slug="datatype_lookups")
@@ -176,6 +313,15 @@ class RestFrameworkTests(GraphTestCase):
             graph_slug="datatype_lookups", nodegroup_alias="datatypes_1"
         )
         self.assertNotIn("datatypes_1_child", serializer.data["aliased_data"])
+
+    def test_blank_views_tile_id(self):
+        response = self.client.get(
+            reverse(
+                "arches_querysets:api-tile-blank",
+                kwargs={"graph": "datatype_lookups", "nodegroup_alias": "datatypes_1"},
+            )
+        )
+        self.assertIsNone(response.json()["tileid"])
 
     def test_blank_views_exclude_children_option(self):
         response = self.client.get(
@@ -214,6 +360,20 @@ class RestFrameworkTests(GraphTestCase):
             QUERY_STRING="exclude_children=true",
         )
         self.assertNotContains(response, "datatypes_1_child")
+
+    def test_fill_blanks_option(self):
+        self.resource_42.tilemodel_set.all().delete()
+        response = self.client.get(
+            reverse(
+                "arches_querysets:api-resource",
+                kwargs={"graph": "datatype_lookups", "pk": str(self.resource_42.pk)},
+            ),
+            QUERY_STRING="fill_blanks=true",
+        )
+        parent_data = response.json()["aliased_data"]["datatypes_1"]
+        self.assertIsNone(parent_data["tileid"])
+        child_data = parent_data["aliased_data"]["datatypes_1_child"]
+        self.assertIsNone(child_data["tileid"])
 
     @patch(
         "arches.app.models.models.UserProfile.viewable_nodegroups",
