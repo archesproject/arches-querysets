648d739c41d9d4ca6d5e854bca8c64ce2b9c0f77
Travis Semple <travis8814@gmail.com>
2026-06-22 07:31:43 -0700

    apply suggestion from jacobtylerwalls

--- arches_querysets/datatypes/geojson.py
+++ arches_querysets/datatypes/geojson.py
@@ -6,12 +6,9 @@ class GeojsonFeatureCollectionDataType(datatypes.GeojsonFeatureCollectionDataTyp
     def after_update_all(self, tile=None, changed_tiles=None):
         if changed_tiles is None:
             return super().after_update_all(tile)
-        geojson_nodegroup_ids = set(
-            Node.objects.filter(
-                nodegroup_id__in={t.nodegroup_id for t in changed_tiles},
-                datatype="geojson-feature-collection",
-            ).values_list("nodegroup_id", flat=True)
-        )
         for tile in changed_tiles:
-            if tile.nodegroup_id in geojson_nodegroup_ids:
+            if any(
+                node.datatype == "geojson-feature-collection"
+                for node in tile.nodegroup.node_set.all()
+            ):
                 super().after_update_all(tile)
